7ade38f58e167c5b657ba0304ec2cf8eb7730441
Cristi <budescristi@gmail.com>
2026-04-01 19:36:00 +0300

    lista dubla 3

---
+++ Probleme/Probleme Suplimentare/lista dubla 3.c
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+typedef struct Nod Nod;
+struct Nod {
+	Masina info;
+	Nod* next;
+	Nod* prev;
+};
+
+typedef struct ListaDubla ListaDubla;
+struct ListaDubla {
+	Nod* prim;
+	Nod* ultim;
+};
+
+Masina initializareMasina(int id, int nrUsi, float pret, const char* model, const char* numeSofer, unsigned char serie) {
+	Masina m;
+	m.id = id;
+	m.nrUsi = nrUsi;
+	m.pret = pret;
+	m.model = (char*)malloc(strlen(model) + 1);
+	strcpy(m.model, model);
+	m.numeSofer = (char*)malloc(strlen(numeSofer) + 1);
+	strcpy(m.numeSofer, numeSofer);
+	m.serie = serie;
+	return m;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi: %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void adaugaMasinaInLista(ListaDubla* lista, Masina masinaNoua) {
+	Nod* nodNou = (Nod*)malloc(sizeof(Nod));
+	nodNou->info = masinaNoua;
+	nodNou->next = NULL;
+	nodNou->prev = lista->ultim;
+	if (lista->ultim) {
+		lista->ultim->next = nodNou;
+	}
+	else {
+		lista->prim = nodNou;
+	}
+	lista->ultim = nodNou;
+}
+
+void stergeMasiniDupaNumarUsi(ListaDubla* lista, int nrUsi) {
+	Nod* p = lista->prim;
+	while (p) {
+		Nod* urmator = p->next;
+		if (p->info.nrUsi == nrUsi) {
+			if (p->prev) {
+				p->prev->next = p->next;
+			}
+			else {
+				lista->prim = p->next;
+			}
+			if (p->next) {
+				p->next->prev = p->prev;
+			}
+			else {
+				lista->ultim = p->prev;
+			}
+			free(p->info.model);
+			free(p->info.numeSofer);
+			free(p);
+		}
+		p = urmator;
+	}
+}
+
+void afisareLista(ListaDubla lista) {
+	Nod* p = lista.prim;
+	while (p) {
+		afisareMasina(p->info);
+		p = p->next;
+	}
+}
+
+void dezalocareLista(ListaDubla* lista) {
+	while (lista->prim) {
+		Nod* temp = lista->prim;
+		lista->prim = lista->prim->next;
+		free(temp->info.model);
+		free(temp->info.numeSofer);
+		free(temp);
+	}
+	lista->ultim = NULL;
+}
+
+int main() {
+	ListaDubla lista;
+	lista.prim = NULL;
+	lista.ultim = NULL;
+
+	adaugaMasinaInLista(&lista, initializareMasina(1, 4, 9000, "Logan", "Ion", 'A'));
+	adaugaMasinaInLista(&lista, initializareMasina(2, 2, 25000, "BMW", "Ana", 'B'));
+	adaugaMasinaInLista(&lista, initializareMasina(3, 4, 18000, "Dacia", "Maria", 'A'));
+	adaugaMasinaInLista(&lista, initializareMasina(4, 5, 16000, "Ford", "Vlad", 'C'));
+
+	stergeMasiniDupaNumarUsi(&lista, 4);
+	afisareLista(lista);
+
+	dezalocareLista(&lista);
+	return 0;
+}
